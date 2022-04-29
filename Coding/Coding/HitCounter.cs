bfbea326627753986cb6a67134322ab09dcebfb9
Anand Kishore <ankisho@microsoft.com>
2022-04-29 18:45:47 +0530

    hit counter queue

--- Coding/Coding/HitCounter.cs
+++ Coding/Coding/HitCounter.cs
@@ -4,12 +4,18 @@ using System.Collections.Generic;
 
 public class HitCounter
 {
-    public Dictionary<long, Dictionary<char, int>> CounterData { get; set; }
+    public Dictionary<long, Dictionary<char, int>> CounterData { get; set; }// using dictionary so that we can get count from past timestamp
+
+    public Dictionary<char, Queue<long>> CounterDataQ { get; set; } 
+    // using Queue, we can only get past 5 min data once. 
+    //if multithreads get count at the same time, then only one thread will get count and next thread will give 0 because we are doing dequeue.
+
     public long TimeRangeInSec { get; set; }
 
     public HitCounter(long timeRangeInSec)
     {
         CounterData = new Dictionary<long, Dictionary<char, int>>();
+        CounterDataQ = new Dictionary<char, Queue<long>>();
         TimeRangeInSec = timeRangeInSec;
     }
 
@@ -36,11 +42,26 @@ public class HitCounter
         }
     }
 
+    public void CountQ(char key, long timeStamp)
+    {
+        if(CounterDataQ.ContainsKey(key))
+        {
+            CounterDataQ[key].Enqueue(timeStamp);
+        }
+        else
+        {
+            var data = new Queue<long>();
+            data.Enqueue(timeStamp);
+
+            CounterDataQ.Add(key, data);
+        }
+    }
+
     public int GetCount(char key, long timeStamp)
     {
         int result = 0;
         long endtime = timeStamp - TimeRangeInSec;
-
+        
         while(timeStamp >= endtime)
         {
             if(CounterData.ContainsKey(timeStamp) && CounterData[timeStamp].ContainsKey(key))
@@ -54,6 +75,23 @@ public class HitCounter
         return result;
     }
 
+    public int GetCountQ(char key, long timeStamp)
+    {
+        int result = 0;
+        long endtime = timeStamp - TimeRangeInSec;
+
+        if(CounterDataQ.ContainsKey(key))
+        {
+            while (CounterDataQ[key] != null && CounterDataQ[key].Count > 0 && CounterDataQ[key].Peek() < endtime)
+            {
+                CounterDataQ[key].Dequeue();
+            }
+
+            result = CounterDataQ[key].Count;
+        }
+        
+        return result;
+    }
     public int GetTotalCount(long timeStamp)
     {
         int result = 0;
@@ -70,6 +108,24 @@ public class HitCounter
             }
 
             timeStamp--;
+        }
+
+        return result;
+    }    
+
+    public int GetTotalCountQ(long timeStamp)
+    {
+        int result = 0;
+        long endtime = timeStamp - TimeRangeInSec;
+
+        foreach (var item in CounterDataQ)
+        {
+            while (item.Value.Count > 0 && item.Value.Peek() < endtime)
+            {
+                item.Value.Dequeue();
+            }
+
+            result += item.Value.Count;
         }
 
         return result;
